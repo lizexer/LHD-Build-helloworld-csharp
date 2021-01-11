4daef4d4f03d4f1f15e70d2887f9f33251b3e405
Lizette Rodriguez <75644668+lizexer@users.noreply.github.com>
2021-01-11 11:44:22 -0600

    First C# program - Hello World!
    Created as part of LHD: BUILD day 1 challenge

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//Lizette Rodriguez
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first time using C#, and I learned from Aditya at MLH!");
+        }
+    }
+}
